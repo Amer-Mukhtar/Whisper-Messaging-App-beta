9fdfdb99271c1627df0498d8babe4c724938396f
Amer-Mukhtar <amermukhtar19@gmail.com>
2025-08-23 22:41:20 -0700

    key change

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,15 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:whisper/config/secrets.dart';
 import 'package:whisper/routes/pages.dart';
 import 'package:whisper/views/Home&Welcome/welcome_screen.dart';
-import 'config/env.dart';
 import 'controller/Theme/theme_controller.dart';
 import 'core/theme/theme.dart';
 
 void main() async {
-  final anonKey = Env.supabaseAnonKey;
-  final supabaseUrl =Env.supabaseUrl;
+  final anonKey = secrets.SUPABASE_ANON_KEY;
+  final supabaseUrl =secrets.SUPABASE_URL;
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await Supabase.initialize(url: supabaseUrl, anonKey: anonKey);
